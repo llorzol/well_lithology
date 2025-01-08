e91b7b2a494ef95dc0fa48cf0301fe61a361081a
llorzol <llorzol@gmail.com>
2025-01-07 20:19:51 -0800

    version 3.10 January 7, 2025

--- htdocs/javascripts/usgs/d3.AxisFunctions.js
+++ htdocs/javascripts/usgs/d3.AxisFunctions.js
@@ -4,8 +4,8 @@
  * d3_AxisFunctions is a JavaScript library to provide a set of functions to build
  *  axes and labelling for well construction and lithology applications in svg format.
  *
- * version 3.09
- * December 30, 2024
+ * version 3.10
+ * January 7, 2025
 */
 
 /*
@@ -356,6 +356,51 @@ function labelWellboreDiameter(
         .text(axis_label);
   }
 
+function buildDefs(svgContainer, listDefs) {
+    myLogger.info("buildDefs");
+    myLogger.info(listDefs);
+
+    // Check for existing definitions section
+    //
+    let defs = d3.select("defs");
+    
+    // Set definitions in svg container if needed
+    //
+    if(defs.size() < 1) {
+        myLogger.info(`Creating definition section defs ${defs.size()}`);
+        defs = svgContainer.append("defs")
+    }
+    else {
+        myLogger.info(`Appending to definition section defs ${defs.size()}`);
+    }
+
+    // Build definitions section
+    //
+    for(let i = 0; i < listDefs.length; i++) {
+
+        let id          = listDefs[i].id;
+        let description = listDefs[i].description;
+        let symbol      = listDefs[i].symbol;
+
+        // Build legend
+        //
+        let pattern = defs.append("pattern")
+            .attr('id', id)
+            .attr('patternUnits', 'userSpaceOnUse')
+            .attr('width', 100)
+            .attr('height', 100)
+
+        let myimage = pattern.append('image')
+            .attr('xlink:href', ["lithology_patterns", symbol].join("/"))
+            .attr('width', 100)
+            .attr('height', 100)
+            .attr('x', 0)
+            .attr('y', 0)
+    }
+
+    return;
+  }
+
 
 // Min and max
 //
@@ -511,329 +556,3 @@ function shadeHexColor(color, percent) {
 
 
 
-
-
-
-
-// Old versions
-
-
-// Left axis
-//
-function leftAxis2(
-                  svgContainer,
-                  x_box_min, 
-                  x_box_max, 
-                  y_box_min,
-                  y_box_max,
-                  y_min,
-                  y_max,
-                  y_interval,
-                  axis_label
-                 )
-  { 
-   myLogger.debug("leftAxis");
-
-   // Determine text width for label placement
-   //
-   var text_label  = String(y_max);
-   var text_length = text_label.length;
-
-   var myText      = svgContainer.append("text")
-                                 .attr("class", "tic_labels")
-                                 .text(text_label);
-   var text_width  = myText.node().getComputedTextLength() / text_label.length;
-   myLogger.debug("text_width " + text_width);
-
-   // Draw y tics and labels
-   //
-   var y           = 0.0
-   var y_range     = y_max - y_min;
-   var y_axis      = y_box_max - y_box_min;
-   myLogger.debug("y_range " + y_range);
-
-   var tics        = svgContainer.append("g")
-                                 .attr("id", "tics")
-                                 .attr("stroke", "black")
-                                 .attr("strokeWidth", 1)
-
-   var tic_labels  = svgContainer.append("g")
-                                 .attr("class", "tic_labels")
-    
-   // Left y tics and labels
-   //
-   while ( y <= y_max ) {
-        y_tic            = y_box_min + y_axis * (y - y_min) / y_range
-
-        // Left tic
-        //
-        var myLine      = tics.append("line")
-                              //.attr('id', lith_cd)
-                              .attr("x1", x_box_min)
-                              .attr("y1", y_tic)
-                              .attr("x2", x_box_min + 10)
-                              .attr("y2", y_tic);
-
-        // Left tic labels
-        //
-        y_label_txt      = String(y);
-        label_x          = x_box_min - text_width;
-        label_y          = y_tic + text_width * 0.50;
-        //myLogger.debug("Max Y " + y_max + " Y " + y);
-
-        var myText       = tic_labels.append("text")
-                                     .attr('x', label_x)
-                                     .attr('y', label_y)
-                                     .attr('class', 'tic_labels')
-                                     .text(y_label_txt);
-     
-        y               += y_interval
-   }
-
-   // Left axis label
-   //
-   var labelOffset = ( text_length + 2 ) * text_width;
-   var label       = "translate("
-   label          += [x_box_min - labelOffset, (y_box_max + y_box_min ) * 0.5].join(", ");
-   label          += ") rotate(-90)";
-
-   var left_axis_label = svgContainer.append("g")
-                                     .append("text")
-                                     .attr("transform", label)
-                                     .attr('class', 'y_axis_label')
-                                     .text(axis_label);
-  }
-
-// Right axis
-//
-function rightAxis2(
-                  svgContainer,
-                  x_box_min, 
-                  x_box_max, 
-                  y_box_min,
-                  y_box_max,
-                  y_min,
-                  y_max,
-                  y_interval,
-                  axis_label,
-                  altitude_accuracy
-                 )
-  { 
-   myLogger.debug("rightAxis");
-   myLogger.debug("y_min " + y_min);
-   myLogger.debug("y_max " + y_max);
-
-   // Determine text width for label placement
-   //
-   var text_label  = y_max.toFixed(altitude_accuracy);
-   var text_length = text_label.length;
-
-   if(y_min.toFixed(altitude_accuracy).length > text_length)
-     {
-      text_label  = y_min.toFixed(altitude_accuracy);
-      text_length = text_label.length;
-     }
-
-   var myText      = svgContainer.append("text")
-                                 .attr("class", "tic_labels")
-                                 .text(text_label);
-   var text_offset = myText.node().getComputedTextLength() / text_label.length;
-   myLogger.debug("text_offset " + text_offset);
-
-   // Draw y tics and labels
-   //
-   var y           = y_max;
-   var y_range     = y_max - y_min;
-   var y_axis      = y_box_max - y_box_min;
-   myLogger.debug("y_range " + y_range);
-
-   var tics        = svgContainer.append("g")
-                                 .attr("id", "tics")
-                                 .attr("stroke", "black")
-                                 .attr("strokeWidth", 1)
-
-   var tic_labels  = svgContainer.append("g")
-                                 .attr("class", "tic_labels")
-    
-   // Right tics and labels
-   //
-   while ( y >= y_min ) {
-        y_tic            = y_box_min + y_axis * (y_max - y) / y_range
-
-        // Right tic
-        //
-        var myLine      = tics.append("line")
-                              //.attr('id', lith_cd)
-                              .attr("x1", x_box_max)
-                              .attr("y1", y_tic)
-                              .attr("x2", x_box_max - 10)
-                              .attr("y2", y_tic);
-
-        // Right tic labels
-        //
-        y_label_txt      = y.toFixed(altitude_accuracy);
-        label_x          = x_box_max + ( text_length + 1 ) * text_offset;
-        label_y          = y_tic + text_offset * 0.50;
-        myLogger.debug("Y " + y + " y_label_txt " + y_label_txt);
-
-        var myText       = tic_labels.append("text")
-                                     .attr('x', label_x)
-                                     .attr('y', label_y)
-                                     .attr('class', 'tic_labels')
-                                     .text(y_label_txt);
-     
-        y               -= y_interval
-   }
-
-   // Right axis label
-   //
-   var labelOffset = ( text_length + 2 ) * text_offset;
-   var label       = "translate("
-   label          += [x_box_max + labelOffset, (y_box_max + y_box_min ) * 0.5].join(", ");
-   label          += ") rotate(90)";
-
-   var left_axis_label = svgContainer.append("g")
-                                     .append("text")
-                                     .attr("transform", label)
-                                     .attr('class', 'y_axis_label')
-                                     .text(axis_label);
-  }
-
-
-// Label y axes
-//
-function label_YAxes(
-                     svgContainer,
-                     x_box_min, 
-                     x_box_max, 
-                     y_box_min,
-                     y_box_max,
-                     y_min,
-                     y_max,
-                     y_interval
-                    )
-  { 
-   myLogger.debug("label_YAxes");
-
-   // Determine text width for label placement
-   //
-   var text_label  = String(y_max);
-   var text_length = text_label.length;
-
-   var myText      = svgContainer.append("text")
-                                 .attr("class", "tic_labels")
-                                 .text(text_label);
-   var text_width  = myText.node().getComputedTextLength() / text_label.length;
-   myLogger.debug("text_width " + text_width);
-
-   // Draw y tics and labels
-   //
-   var y           = 0.0
-   var y_range     = y_max - y_min;
-
-   var tics        = svgContainer.append("g")
-                                 .attr("id", "tics")
-                                 .attr("stroke", "black")
-                                 .attr("strokeWidth", 1)
-
-   var tic_labels  = svgContainer.append("g")
-                                 .attr("class", "tic_labels")
-    
-   // Left y tics and labels
-   //
-   while ( y <= y_max ) {
-        y_tic            = y_box_min + y_axis * (y - y_min) / y_range
-
-        // Left tic
-        //
-        var myLine      = tics.append("line")
-                              //.attr('id', lith_cd)
-                              .attr("x1", x_box_min)
-                              .attr("y1", y_tic)
-                              .attr("x2", x_box_min + 10)
-                              .attr("y2", y_tic);
-
-        // Left tic labels
-        //
-        y_label_txt      = String(y);
-        label_x          = x_box_min - text_width;
-        label_y          = y_tic + text_width * 0.50;
-        //myLogger.debug("Max Y " + y_max + " Y " + y);
-
-        var myText       = tic_labels.append("text")
-                                     .attr('x', label_x)
-                                     .attr('y', label_y)
-                                     .attr('class', 'tic_labels')
-                                     .text(y_label_txt);
-     
-        y               += y_interval
-   }
-
-   // Left axis label
-   //
-   label_txt       = "Depth Below Land Surface, in feet";
-   var labelOffset = ( text_length + 2 ) * text_width;
-   var label       = "translate("
-   label          += [x_box_min - labelOffset, (y_box_max + y_box_min ) * 0.5].join(", ");
-   label          += ") rotate(-90)";
-
-   var left_axis_label = svgContainer.append("g")
-                                     .append("text")
-                                     .attr("transform", label)
-                                     .attr('class', 'y_axis_label')
-                                     .text(label_txt);
-
-   // Prepare
-   //
-   elevation_max   = land_surface;
-   var elev_offset = String(elevation_max).length;
-   elevation_max   = land_surface - y_max;
-   if(elev_offset < elevation_max.toFixed(altitude_accuracy).length)
-     {
-      elev_offset = elevation_max.toFixed(altitude_accuracy).length;
-     }
-   label_offset    = ( elev_offset + 1 ) * text_width;
-
-   // Right y tics and labels
-   //
-   var y         = 0.0
-   while ( y <= y_max ) {
-        y_tic            = y_box_min + y_axis * (y - y_min) / y_range
-
-        // Right tics
-        //
-        var myLine      = tics.append("line")
-                              //.attr('id', lith_cd)
-                              .attr("x1", x_box_max)
-                              .attr("y1", y_tic)
-                              .attr("x2", x_box_max - 10)
-                              .attr("y2", y_tic);
-             
-        // Right tic labels
-        //
-        y_label_txt      = land_surface.toFixed(altitude_accuracy);
-        label_x          = x_box_max + label_offset;
-        label_y          = y_tic + text_size * 0.50;
-        var myText       = tic_labels.append("text")
-                                     .attr('x', label_x)
-                                     .attr('y', label_y)
-                                     .attr('class', 'tic_labels')
-                                     .text(y_label_txt);
-     
-        y               += y_interval
-        land_surface    -= y_interval
-   }
-
-   // Right axis label
-   //
-   label_txt       = "Elevation, in feet";
-   var  label      = "translate("
-   label          += [x_box_max + label_offset + text_width * 2, (y_box_max + y_box_min ) * 0.5].join(", ");
-   label          += ") rotate(90)";
-
-   var right_axis_label = svgContainer.append("g")
-                                      .append("text")
-                                      .attr("transform", label)
-                                      .attr('class', 'y_axis_label')
-                                      .text(label_txt);
-  }
